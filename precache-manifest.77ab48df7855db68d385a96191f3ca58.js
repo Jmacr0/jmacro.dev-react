d68e3326604c59b4c53b53ae80141b1eba368fc0
Jmacro <jonmacrohon@gmail.com>
2020-03-13 00:40:00 +1100

    Updates

---
+++ precache-manifest.77ab48df7855db68d385a96191f3ca58.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01959a4ead7b1d07c3b3524630c6428",
+    "url": "/jmacro.dev-react/index.html"
+  },
+  {
+    "revision": "6e2fac216ca31af11c7a",
+    "url": "/jmacro.dev-react/static/css/main.8f2bdf60.chunk.css"
+  },
+  {
+    "revision": "cc009a5a1813658bf369",
+    "url": "/jmacro.dev-react/static/js/2.2b683c58.chunk.js"
+  },
+  {
+    "revision": "a2e26c9252dea7ee90077b51746a1553",
+    "url": "/jmacro.dev-react/static/js/2.2b683c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2fac216ca31af11c7a",
+    "url": "/jmacro.dev-react/static/js/main.321a34cd.chunk.js"
+  },
+  {
+    "revision": "117dc6fca4ac285e5b7e",
+    "url": "/jmacro.dev-react/static/js/runtime-main.777df0d4.js"
+  },
+  {
+    "revision": "d4b6fc04a211d071cefb080f903b94d7",
+    "url": "/jmacro.dev-react/static/media/blogger.d4b6fc04.png"
+  },
+  {
+    "revision": "3ca96827e4d432f02e73a63b504c9105",
+    "url": "/jmacro.dev-react/static/media/me-background.3ca96827.jpg"
+  },
+  {
+    "revision": "0ed7a4be7994b66d284a6a4cf50d604e",
+    "url": "/jmacro.dev-react/static/media/phone.0ed7a4be.png"
+  },
+  {
+    "revision": "2791c2efe89c72dafd9d2696d0801354",
+    "url": "/jmacro.dev-react/static/media/read-me.2791c2ef.png"
+  },
+  {
+    "revision": "0b3f9eeebe663f34868b0f78b38eb38a",
+    "url": "/jmacro.dev-react/static/media/river-background.0b3f9eee.jpg"
+  },
+  {
+    "revision": "00dc0de315c0dff17dbcf84adbcf60c8",
+    "url": "/jmacro.dev-react/static/media/what-the-hex-dark.00dc0de3.png"
+  }
+]);
